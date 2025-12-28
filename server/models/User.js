fddceae2cb466080b133c224be706ea2960af894
Alen-prjcts <prjcts3010@gmail.com>
2025-12-28 16:02:25 +0530

    Updated Login/Regn To one

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: { type: String },
+  name: {type: String},
+  email: { type: String,
+   required: true,
+   unique: true },
+
   otp: { type: String },
-  email: { type: String },
   subscriptionType: { type: String }
 });
 
